61da1c717a0559de5fbb725f45ffe73807a91808
nisma687 <20-41982-1@student.aiub.edu>
2024-01-03 21:28:07 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {
+ 
+  RouterProvider,
+} from "react-router-dom";
+import router from './router/router.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='mx-auto max-w-6xl'>
+    <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+  </div>
+)
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-2xl font-semibold mt-2">This is home page</h3>
+        </div>
+    );
+};
+
+export default Home;
